bb78e1bc0aa4d5de735371f0929f9777dd0ba961
Fahad Altaf <fahadaltaf10se49@gmail.com>
2020-11-05 18:21:31 +0500

    First Release

--- indeed.com.scraper/Program.cs
+++ indeed.com.scraper/Program.cs
@@ -1,52 +1,120 @@
-﻿using HtmlAgilityPack;
+﻿using CsvHelper;
+using HtmlAgilityPack;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace indeed.com.scraper
 {
+    public class DataModel
+    {
+        public string SearchedKeyword { get; set; }
+        public string SearchedLocation { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string CompanyName { get; set; }
+        public string ShortDescription { get; set; }
+        public string Location { get; set; }
+        public string PostTime { get; set; }
+        public string Rating { get; set; }
+        public string CurrentTime { get { return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); } }
+    }
     class Program
     {
-        static List<string> entries = new List<string>();
+        static string keyword = "director of rehabilitation";
+        static string searchedLocation = "United States";
+        static List<DataModel> entries = new List<DataModel>();
         static void Main(string[] args)
         {
-            using (var driver = new ChromeDriver())
+            try
             {
-                Console.WriteLine("Loading https://www.indeed.com/...");
-                driver.Navigate().GoToUrl("https://www.indeed.com/");
+                Console.WriteLine("Enter your search query: ");
+                keyword = Console.ReadLine();
+                Console.WriteLine("Enter location to be searched: ");
+                searchedLocation = Console.ReadLine();
 
-                driver.FindElement(By.Id("text-input-what")).SendKeys("director of rehabilitation");
-                driver.FindElement(By.Id("text-input-where")).SendKeys(Keys.Control + "a");
-                driver.FindElement(By.Id("text-input-where")).SendKeys("United States");
-                driver.FindElement(By.XPath("//*[@id=\"whatWhereFormId\"]/div[3]/button")).Click();
-
-                Thread.Sleep(3000);
-
-                HtmlDocument doc = new HtmlDocument();
-                doc.LoadHtml(driver.PageSource);
-
-                var count = doc.DocumentNode.SelectSingleNode("//div[@id='searchCountPages']");
-                var total = Convert.ToInt32(count.InnerText.Split(new string[] { "of" }, StringSplitOptions.RemoveEmptyEntries)[1].Replace("jobs", "").Replace(",", "").Trim());
-              //  ExtractData(doc);
-                int pages = (total + 50 - 1) / 50;
-                string url = driver.Url;
-                for (int i = 0; i < pages; i++)
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments((IEnumerable<string>)new List<string>()
                 {
-                    Console.WriteLine($"Processing page {i+1}");
-                    string start = $"&start={(i * 50)}&limit=50";
-                    var newUrl = url.Split(new string[] { "&vjk" },StringSplitOptions.RemoveEmptyEntries)[0] + start;
-                    driver.Navigate().GoToUrl(newUrl);
+                        "--silent-launch",
+                        "--no-startup-window",
+                        "no-sandbox",
+                        "headless",
+                        "incognito"
+                });
+
+                ChromeDriverService defaultService = ChromeDriverService.CreateDefaultService();
+                defaultService.HideCommandPromptWindow = true;
+
+                using (IWebDriver driver = (IWebDriver)new ChromeDriver(defaultService, options))
+                {
+                    Console.WriteLine("Loading https://www.indeed.com/...");
+                    driver.Navigate().GoToUrl("https://www.indeed.com/");
+
+                    driver.FindElement(By.Id("text-input-what")).SendKeys(keyword);
+                    driver.FindElement(By.Id("text-input-where")).SendKeys(Keys.Control + "a");
+                    driver.FindElement(By.Id("text-input-where")).SendKeys(searchedLocation);
+                    driver.FindElement(By.XPath("//*[@id=\"whatWhereFormId\"]/div[3]/button")).Click();
+
                     Thread.Sleep(3000);
-                    doc = new HtmlDocument();
+
+                    HtmlDocument doc = new HtmlDocument();
                     doc.LoadHtml(driver.PageSource);
-                    ExtractData(doc);
+
+                    var count = doc.DocumentNode.SelectSingleNode("//div[@id='searchCountPages']");
+                    if (count == null) { Console.WriteLine("No results found against your search."); }
+                    else
+                    {
+                        var total = Convert.ToInt32(count.InnerText.Split(new string[] { "of" }, StringSplitOptions.RemoveEmptyEntries)[1].Replace("jobs", "").Replace(",", "").Trim());
+                        //  ExtractData(doc);
+                        int pages = (total + 50 - 1) / 50;
+                        string url = driver.Url;
+                        for (int i = 0; i < pages; i++)
+                        {
+                            Console.WriteLine($"Processing page {i + 1}");
+                            string start = $"&start={(i * 50)}&limit=50";
+                            var newUrl = url.Split(new string[] { "&vjk" }, StringSplitOptions.RemoveEmptyEntries)[0] + start;
+                            driver.Navigate().GoToUrl(newUrl);
+                            Thread.Sleep(3000);
+                            doc = new HtmlDocument();
+                            doc.LoadHtml(driver.PageSource);
+                            ExtractData(doc);
+                        }
+                    }
+
+                    driver.Close();
+                    driver?.Dispose();
                 }
+
+                if (entries.Count > 0)
+                {
+                    var today = DateTime.Now;
+                    string name = $"{today.Year}{today.Month}{today.Day}{today.Hour}{today.Minute}{today.Second}.csv";
+                    using (var writer = new StreamWriter(name))
+                    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    {
+                        csv.WriteRecords(entries);
+                    }
+                    Console.WriteLine("Data exported successfully");
+                }
+                else
+                    Console.WriteLine("We have nothing to export");
+
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("We are unable to continue. Reason: " + ex.Message);
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
 
         private static void ExtractData(HtmlDocument doc)
@@ -59,15 +127,62 @@ namespace indeed.com.scraper
             if (nodes != null && nodes.Count > 0)
                 foreach (var record in nodes)
                 {
-                    var detailsDoc = new HtmlDocument();
-                    detailsDoc.LoadHtml(record.InnerHtml);
-                    var title = detailsDoc.DocumentNode.SelectSingleNode("/h2[1]/a[1]");
-                    if (title != null)
+                    try
                     {
-                        entries.Add(title.Attributes.FirstOrDefault(x=>x.Name=="href").Value);
-                        Console.WriteLine(title.Attributes.FirstOrDefault(x => x.Name == "href").Value);
-                    }
+                        var entry = new DataModel() { SearchedKeyword = keyword, SearchedLocation = searchedLocation };
+                        var detailsDoc = new HtmlDocument();
+                        detailsDoc.LoadHtml(record.InnerHtml);
+                        var title = detailsDoc.DocumentNode.SelectSingleNode("//h2[@class='title']");
+                        if (title != null)
+                        {
+                            entry.Url = "https://www.indeed.com" + title.ChildNodes[1].Attributes.FirstOrDefault(x => x.Name == "href").Value;
+                            entry.Title = HttpUtility.HtmlDecode(title.InnerText.Replace("\nnew", "").Replace("\n", "").Replace("\r", ""));
+                            Console.WriteLine(title.InnerText);
+                        }
 
+                        var company = detailsDoc.DocumentNode.SelectSingleNode("//span[@class='company']");
+                        if (company != null)
+                        {
+                            entry.CompanyName = HttpUtility.HtmlDecode(company.InnerText.Replace("\n", "").Replace("\r", ""));
+
+                        }
+
+                        var rating = detailsDoc.DocumentNode.SelectSingleNode("//span[@class='ratingsContent']");
+                        if (rating == null)
+                        {
+                            rating = detailsDoc.DocumentNode.SelectSingleNode("//div[@class='ratingsContent']");
+                        }
+                        if (rating != null)
+                        {
+                            entry.Rating = HttpUtility.HtmlDecode(rating.InnerText.Replace("\n", "").Replace("\r", ""));
+                        }
+
+                        var location = detailsDoc.DocumentNode.SelectSingleNode("//span[@class='location accessible-contrast-color-location']");
+                        if (location == null)
+                        {
+                            location = detailsDoc.DocumentNode.SelectSingleNode("//div[@class='location accessible-contrast-color-location']");
+                        }
+                        if (location != null)
+                        {
+                            entry.Location = HttpUtility.HtmlDecode(location.InnerText.Replace("\n", "").Replace("\r", ""));
+                        }
+                        var description = detailsDoc.DocumentNode.SelectSingleNode("//div[@class='summary']");
+                        if (description != null)
+                        {
+                            entry.ShortDescription = HttpUtility.HtmlDecode(description.InnerText.Replace("\n", "").Replace("\r", ""));
+                        }
+                        var postedOn = detailsDoc.DocumentNode.SelectSingleNode("//span[@class='date ']");
+                        if (postedOn != null)
+                        {
+                            entry.PostTime = HttpUtility.HtmlDecode(postedOn.InnerText.Replace("\n", "").Replace("\r", ""));
+                        }
+
+                        entries.Add(entry);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Unable to extract details. Reason: " + ex.Message);
+                    }
                 }
         }
     }
