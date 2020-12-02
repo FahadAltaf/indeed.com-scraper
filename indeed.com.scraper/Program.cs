8b84020f119d335176b17babed80e980b3564412
Fahad Altaf <fahadaltaf10se49@gmail.com>
2020-12-02 21:39:10 +0500

    filters added

--- indeed.com.scraper/Program.cs
+++ indeed.com.scraper/Program.cs
@@ -27,12 +27,15 @@ namespace indeed.com.scraper
         public string Location { get; set; }
         public string PostTime { get; set; }
         public string Rating { get; set; }
-        public string CurrentTime { get { return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); } }
+        public string FullDescription { get; set; }
+        public string CurrentTime { get; set; }
     }
     class Program
     {
-        static string keyword = "director of rehabilitation";
+        static string keyword = "epic analyst";
         static string searchedLocation = "United States";
+        static bool getDescription = false;
+        static string age = "";
         static List<DataModel> entries = new List<DataModel>();
         static void Main(string[] args)
         {
@@ -42,15 +45,29 @@ namespace indeed.com.scraper
                 keyword = Console.ReadLine();
                 Console.WriteLine("Enter location to be searched: ");
                 searchedLocation = Console.ReadLine();
+                
+                Console.WriteLine("Do you want to scrape full description? (if yes press y else just press enter): ");
+                var selection = Console.ReadLine();
+
+                if (selection.ToLower() == "y")
+                    getDescription = true;
+
+                Console.WriteLine("Select Rnage");
+                Console.WriteLine("Today(Select 1)");
+                Console.WriteLine("3 Days(Select 3)");
+                Console.WriteLine("7 Days(Select 7)");
+                Console.WriteLine("14 Days(Select 14)");
+                Console.WriteLine("To ignnore just press enter.");
+                age = Console.ReadLine();
 
                 ChromeOptions options = new ChromeOptions();
                 options.AddArguments((IEnumerable<string>)new List<string>()
                 {
-                        "--silent-launch",
-                        "--no-startup-window",
-                        "no-sandbox",
-                        "headless",
-                        "incognito"
+                        //"--silent-launch",
+                        //"--no-startup-window",
+                        //"no-sandbox",
+                        //"headless",
+                        //"incognito"
                 });
 
                 ChromeDriverService defaultService = ChromeDriverService.CreateDefaultService();
@@ -84,16 +101,34 @@ namespace indeed.com.scraper
                         {
                             Console.WriteLine($"Processing page {i + 1}");
                             string start = $"&start={(i * 50)}&limit=50";
+                            if(!string.IsNullOrEmpty(age))
+                                switch (age)
+                                {
+                                    case "1":
+                                        start += "&fromage=1";
+                                        break;
+                                    case "3":
+                                        start += "&fromage=3";
+                                        break;
+                                    case "7":
+                                        start += "&fromage=7";
+                                        break;
+                                    case "14":
+                                        start += "&fromage=14";
+                                        break;
+                                    default:
+                                        break;
+                                }
                             var newUrl = url.Split(new string[] { "&vjk" }, StringSplitOptions.RemoveEmptyEntries)[0] + start;
                             driver.Navigate().GoToUrl(newUrl);
                             Thread.Sleep(3000);
-                            doc = new HtmlDocument();
-                            doc.LoadHtml(driver.PageSource);
-                            ExtractData(doc);
+
+                            ExtractData(driver);
                         }
                     }
 
                     driver.Close();
+                    Thread.Sleep(3000);
                     driver?.Dispose();
                 }
 
@@ -120,19 +155,23 @@ namespace indeed.com.scraper
             Console.ReadKey();
         }
 
-        private static void ExtractData(HtmlDocument doc)
+        private static void ExtractData(IWebDriver driver)
         {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(driver.PageSource);
 
             var nodes = doc.DocumentNode.SelectNodes("//div[@class='jobsearch-SerpJobCard unifiedRow row result clickcard']");
             var node = doc.DocumentNode.SelectSingleNode("//div[@class='jobsearch-SerpJobCard unifiedRow row result clickcard vjs-highlight']");
             if (nodes != null && node != null)
-                nodes.Add(node);
+                nodes.Insert(0,node);
             if (nodes != null && nodes.Count > 0)
                 foreach (var record in nodes)
                 {
+                    Thread.Sleep(1000);
                     try
                     {
-                        var entry = new DataModel() { SearchedKeyword = keyword, SearchedLocation = searchedLocation };
+                        var entry = new DataModel() { SearchedKeyword = keyword, SearchedLocation = searchedLocation, CurrentTime= DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
+                    };
                         var detailsDoc = new HtmlDocument();
                         detailsDoc.LoadHtml(record.InnerHtml);
                         var title = detailsDoc.DocumentNode.SelectSingleNode("//h2[@class='title']");
@@ -140,7 +179,7 @@ namespace indeed.com.scraper
                         {
                             entry.Url = "https://www.indeed.com" + title.ChildNodes[1].Attributes.FirstOrDefault(x => x.Name == "href").Value;
                             entry.Title = HttpUtility.HtmlDecode(title.InnerText.Replace("\nnew", "").Replace("\n", "").Replace("\r", ""));
-                            Console.WriteLine(title.InnerText);
+                            Console.WriteLine(entry.Title);
                         }
 
                         var company = detailsDoc.DocumentNode.SelectSingleNode("//span[@class='company']");
@@ -179,11 +218,30 @@ namespace indeed.com.scraper
                         {
                             entry.PostTime = HttpUtility.HtmlDecode(postedOn.InnerText.Replace("\n", "").Replace("\r", ""));
                         }
+                        if (getDescription)
+                        {
+                            if (!record.Attributes.FirstOrDefault(x=>x.Name=="class").Value.Contains("vjs-highlight"))
+                            {
+                                driver.FindElement(By.XPath(record.XPath)).Click();
+                                Thread.Sleep(5000);
+                            }
+                            
 
+                            driver.SwitchTo().Frame(driver.FindElement(By.Id("vjs-container-iframe")));
+                            detailsDoc.LoadHtml(driver.PageSource);
+                            var descriptionNode = detailsDoc.DocumentNode.SelectSingleNode("//*[@id=\"jobDescriptionText\"]");
+                            if (descriptionNode != null)
+                            {
+                                entry.FullDescription = HttpUtility.HtmlDecode(descriptionNode.InnerText);
+                            }
+                            driver.SwitchTo().DefaultContent();
+                        }
+                        
                         entries.Add(entry);
                     }
                     catch (Exception ex)
                     {
+                        
                         Console.WriteLine("Unable to extract details. Reason: " + ex.Message);
                     }
                 }
